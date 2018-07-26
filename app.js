89a2340e3b1bc548b550f983609acdc27814f133
pavloldev6 <pavlol@dev6.com>
2018-07-25 23:10:46 -0400

    added more details button, shared button

--- app.js
+++ app.js
@@ -196,7 +196,25 @@ async function sendMoreDetailsTemplate(recipientId, clubName) {
           template_type: "generic",
           elements: [
             {
-
+              "title":"Welcome!",
+              "image_url":"https://petersfancybrownhats.com/company_image.png",
+              "subtitle":"We have the right hat for everyone.",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://petersfancybrownhats.com/view?item=103",
+                "webview_height_ratio": "tall",
+              },
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://petersfancybrownhats.com",
+                  "title":"View Website"
+                },{
+                  "type":"postback",
+                  "title":"Start Chatting",
+                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                }              
+              ]      
             }
           ]
         }
